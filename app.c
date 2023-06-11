82863c4a5694875cca401e844591bed5440bc536
Abdul Latif Mubasir <bj.angel119@gmail.com>
2023-06-11 15:24:07 +0700

    fix space

--- app.c
+++ app.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main(){
-	int umur;
-	char nama[60];
-	char ice[20];
-	printf("Mari Belajar Algoritma dengan Bahasa C \n");
-	printf("========================================\n\n");
-	printf("Masukkan nama anda[13..52] : ");
-	scanf("%s",nama);
-	fflush(stdin);
-	printf("Masukkan umur anda : ");
-	scanf("%d",&umur);
-	fflush(stdin);
-	
-	printf("\nBelajar algoritma itu menyenangkan seperti makan es krim \n");
-	printf("Rasa Es Krim yang disukai : ");
-	scanf("%s",ice);
+  int umur;
+  char nama[60];
+  char ice[20];
+  printf("Mari Belajar Algoritma dengan Bahasa C \n");
+  printf("========================================\n\n");
+  printf("Masukkan nama anda[13..52] : ");
+  scanf("%s",nama);
+  fflush(stdin);
+  printf("Masukkan umur anda : ");
+  scanf("%d",&umur);
+  fflush(stdin);
+
+  printf("\nBelajar algoritma itu menyenangkan seperti makan es krim \n");
+  printf("Rasa Es Krim yang disukai : ");
+  scanf("%s",ice);
   printf("================================================================\n\n");
-	printf("%s berumur %d tahun menyukai es krim rasa %s\n\n",nama,umur,ice);
-	printf("    .-\"`'\"-.\n");
+  printf("%s berumur %d tahun menyukai es krim rasa %s\n\n",nama,umur,ice);
+  printf("    .-\"`'\"-.\n");
   printf("   /        \\\n");
   printf("   |        |\n");
   printf("   /'---'--`\\\n");
@@ -29,10 +29,10 @@ int main(){
   printf("     \\=-=-/\n");
   printf("      \\=-/\n");
   printf("       \\/\n");
-	printf("\n");
+  printf("\n");
   printf("SELAMAT BELAJAR ALGORITMA !\n\n");
-	
+
   fflush(stdin);
-	getchar();
-	return 0;
+  getchar();
+  return 0;
 }
